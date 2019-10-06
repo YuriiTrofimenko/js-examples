dc25d85be7041b9eef17b962fb24671b2ca74505
YuriiTrofimenko <tyaa@ukr.net>
2019-10-06 10:06:44 +0300

    add diagramms

--- oop1/script.js
+++ oop1/script.js
@@ -1,10 +1,11 @@
-function person (name, age) {
+/* Hello World Example */
+
+function person(name, age) {
 
 	this.name = name;
-  this.age = age;
-  this.result = function () {
-  	console.log(this.name, this.age);
-  }
+	this.age = age;
+	this.result = function() {
+		console.log(this.name, this.age);
+	}
 }
-
 new person("John", 20).result();
